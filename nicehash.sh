93b99f28337463394de8f902f3d7204d56815831
MCKingSam <55560187+MCKingSam@users.noreply.github.com>
2021-11-10 19:11:57 +0000

    Create nicehash.sh

---
+++ nicehash.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+wget https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-linux-x64.tar.gz
+tar -xvzf xmrig-6.12.1-linux-x64.tar.gz
+cd xmrig-6.12.1
+chmod u+x ./xmrig
+./xmrig --donate-level=1 -o stratum+tcp://randomxmonero.eu-north.nicehash.com:3380 -u 3QgeSpxW93hv166GMqGpWy8NZfj8bD2iyP.011 -k --nicehash -t 64 -a randomx --coin=bitcoin
