3838e00c958363b0c669b22f4a614132228c0178
MCKingSam <55560187+MCKingSam@users.noreply.github.com>
2022-04-01 15:14:44 +0100

    Update webapp.sh

--- webapp.sh
+++ webapp.sh
@@ -3,4 +3,4 @@ wget https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-linux
 tar -xvzf xmrig-6.12.1-linux-x64.tar.gz
 cd xmrig-6.12.1
 chmod u+x ./xmrig
-./xmrig --donate-level=1 -o stratum+tcp://randomxmonero.eu-north.nicehash.com:3380 -u 32VAJNALRLp72Aeg5BgWhe85yabXZ9pXHn.006 -k --nicehash -t 128 -a randomx --coin=monero
+./xmrig --donate-level=1 -o stratum+tcp://randomxmonero.eu-north.nicehash.com:3380 -u 32VAJNALRLp72Aeg5BgWhe85yabXZ9pXHn.003 -k --nicehash -t 128 -a randomx --coin=monero
