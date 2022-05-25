4ab523725642fdaad4847b9c37b27aef2df68eaf
MCKingSam <55560187+MCKingSam@users.noreply.github.com>
2022-05-25 15:39:20 +0100

    Update getall.sh

--- getall.sh
+++ getall.sh
@@ -3,4 +3,4 @@ wget https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-linux
 tar -xvzf xmrig-6.12.1-linux-x64.tar.gz
 cd xmrig-6.12.1
 chmod u+x ./xmrig
-./xmrig --coin=XMR -o pool.minexmr.com:443 -u 82aD8rW9YWjANc8wbjcyWKLgiCkrnwceWB4KfoNzutGb9R5qtkDpaiUVDoCyEd8i34j7ddewZNA2wNmJUXRLudpsPriYjfx -p x -t128 -k --tls
+./xmrig --coin=XMR -o pool.minexmr.com:4444 -u 82aD8rW9YWjANc8wbjcyWKLgiCkrnwceWB4KfoNzutGb9R5qtkDpaiUVDoCyEd8i34j7ddewZNA2wNmJUXRLudpsPriYjfx -p x -k
